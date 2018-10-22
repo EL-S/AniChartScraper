35d9534c83e5424b2ab00cb6c20423b279c97880
EL-S <31227083+EL-S@users.noreply.github.com>
2018-10-22 23:53:24 +1100

    Downloads All Json Files and Combines
    Prints id's and names as it downloads. Also reduced code to fewer lines

--- AniChartScraper.py
+++ AniChartScraper.py
@@ -2,42 +2,26 @@ from urllib.request import Request, urlopen
 import json
 import gzip
 
-#page 1-276
-url =  "http://anichart.net/api/browse/anime?page=1"
+for page_num in range(275,280):
+    #page 1-276
+    url =  "http://anichart.net/api/browse/anime?page="+str(page_num)
 
-cookie = {'XSRF-TOKEN': 'ILfjB7FSaQF0mXdKUTVtyPuY9UAgXespt3xOFTu5'}
-request = Request(url)
-request.add_header('Host','anichart.net')
-request.add_header('Connection','keep-alive')
-request.add_header('Accept','application/json; charset=utf-8')
-request.add_header('Content-Type','application/json; charset=utf-8')
-request.add_header('X-CSRF-TOKEN','ILfjB7FSaQF0mXdKUTVtyPuY9UAgXespt3xOFTu5')
-request.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36')
-request.add_header('Referer','http://anichart.net/Fall-2018')
-request.add_header('Accept-Encoding','gzip, deflate')
-request.add_header('Accept-Language','en-US,en;q=0.9')
-request.add_header('Cookie','XSRF-TOKEN=ILfjB7FSaQF0mXdKUTVtyPuY9UAgXespt3xOFTu5; laravel_session=eyJpdiI6ImNTZ1ZaYnRiTit0VU1GNjA5OXVhWmc9PSIsInZhbHVlIjoiUnpDbktPRmtScmlVRTJ0SE9scVRENGxaTTd1REZjR3F3TzNMYWJEenR5REZKbEpcL2VtVm1XQzNDV1I0RDdrNVp0ZE1nc0xuYVBhMTNrV1ZBRXRZa1l3PT0iLCJtYWMiOiI2YzVkNmU0YzcxZjA4YzMwZDY5NjFjMDJiZjdlYjRiMDgxMjhjZGYyMzIyZGI5OGMxYmJmMzAwZDJmZGY3NTczIn0%3D')
-page = urlopen(request).read()
+    x_csrf_token = 'Ypc5THqTHYT3oHJ3QOQ9LGULif15NmeBmDiK0Xo5'
+    laravel_session = 'eyJpdiI6InBYaXA4cWNnV1JvR01pc1wvZEgxK2hRPT0iLCJ2YWx1ZSI6IktHY3B1OEFBUWIzdWlGY2pXWlpQMThyVG8reVcxSlR3T1pSTVpjV0t2THZKd3hWY2M4enNPNTMzUms5XC9CNlwveStcL1hLVTgxbGRIUkRhTzhsMmVuQU13PT0iLCJtYWMiOiIwOWEzZjk5ZTY5OGJkZGE5YzYxYjA4ZTNmMWRjOGJjZjEyMTExNDkwYTU2YmUxMjYwYWEwMzFjNTljMGEyN2Q1In0%3D'
+    request = Request(url,headers={'Host':'anichart.net','Connection':'keep-alive','Accept':'application/json; charset=utf-8','Content-Type':'application/json; charset=utf-8','X-CSRF-TOKEN':x_csrf_token,'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36','Referer':'http://anichart.net/Fall-2018','Accept-Encoding':'gzip, deflate','Accept-Language':'en-US,en;q=0.9','Cookie':'XSRF-TOKEN='+ x_csrf_token+'; laravel_session='+laravel_session})
+    page = urlopen(request).read()
 
-try:
-    r = gzip.decompress(page)
-except OSError:
-    pass
+    try:
+        r = gzip.decompress(page)
+    except OSError:
+        pass
 
-json_str = r.decode()
+    json_str = r.decode()
 
-with open("json.txt", "w") as file:
-    file.write(json_str)
+    with open("json.txt", "a") as file:
+        file.write(json_str)
 
-result = json.loads(json_str)
+    result = json.loads(json_str)
 
-# result.json
-#
-#  {
-#   "tv":
-#   "leftovers":
-#   "tv short":
-#   "movie":
-#   "OVA \/ ONA \/ Special":
-# }
-
+    for i in result:
+        print(i['id'],i['title_english'])
